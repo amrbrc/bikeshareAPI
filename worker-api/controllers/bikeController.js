7a933452cc287ea3fe81ccc548b217201a02f469
Amer Talastasin <amerburac28@gmai.com>
2026-07-03 09:21:22 +0800

    shorten dispute alerts to single sms message

--- worker-api/controllers/bikeController.js
+++ worker-api/controllers/bikeController.js
@@ -850,7 +850,7 @@ const broken = async (req, res) => {
                     // Alert the previous user by queueing the outbound alert SMS
                     await smsService.queueSMS(
                         prevMemberPhone,
-                        `ALERT: Bike ${bicycleCode} was reported broken by the next user. Your points are frozen. Send a photo of the bike to m(.)me/upbikesharebot (remove parenthesis) or visit UP Bikeshare Admin Hub to appeal`,
+                        `ALERT: Bike ${bicycleCode} reported broken! Points frozen. Send photo to m(.)me/upbikesharebot (remove parenthesis) or visit UPBS Admin Hub to appeal.`,
                         upbsConn
                     );
                 }
@@ -944,7 +944,7 @@ const missing = async (req, res) => {
                 // Alert the previous user by queueing the outbound alert SMS
                 await smsService.queueSMS(
                     prevMemberPhone,
-                    `ALERT: Bike ${bicycleCode} was reported MISSING by the next user. Your points are frozen. Send a photo of the bike to m(.)me/upbikesharebot (remove parenthesis) or visit UP Bikeshare Admin Hub to appeal`,
+                    `ALERT: Bike ${bicycleCode} reported MISSING! Points frozen. Send photo to m(.)me/upbikesharebot (remove parenthesis) or visit UPBS Admin Hub to appeal.`,
                     upbsConn
                 );
             }
